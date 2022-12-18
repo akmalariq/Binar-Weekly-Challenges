ae37c3d18bcbdd2aedb8fb76acc42a6f7282e0ad
akmalariq <santoso54321@gmail.com>
2022-12-18 16:28:40 +0700

    react-bootstrap getting started

--- challenge-07/cari-mobil/src/components/GettingStartedSectionLandingPage.jsx
+++ challenge-07/cari-mobil/src/components/GettingStartedSectionLandingPage.jsx
@@ -1,23 +1,26 @@
 // Dependencies
+import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
 
 export default function GettingStartedSectionLandingPage() {
   return (
       <div className="GettingStartedSectionLandingPage">
-          <div class="container text-center text-white my-5 py-3 px-lg-0">
-        <div class="col rounded-3 py-5 px-5" style={{backgroundColor: "#0D28A6"}}>
-            <div class="col my-3">
-                <h1 class="fw-bold mx-4 mx-lg-0">Sewa Mobil di (Lokasimu) Sekarang</h1>
-                <p class="my-lg-3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br/>
-                    tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <a class="btn text-white my-3 my-lg-4" href="/" role="button" style={{backgroundColor: "#5CB85F"}}>Mulai
-                    Sewa
-                    Mobil</a>
-            </div>
-        </div>
-          </div>
           
+        <Container className="text-center text-white my-5 py-3 px-lg-0">
+            <Col className="rounded-3 py-5 px-5" style={{backgroundColor: "#0D28A6"}}>
+                <Col className="my-3">
+                    <h1 className="fw-bold mx-4 mx-lg-0">Sewa Mobil di (Lokasimu) Sekarang</h1>
+                    <p className="my-lg-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br/>
+                        tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <a className="btn text-white my-3 my-lg-4" href="/" role="button" style={{backgroundColor: "#5CB85F"}}>Mulai
+                        Sewa Mobil
+                    </a>
+                </Col>
+            </Col>
+        </Container>
+
     </div>
   );
 }
